c276a4c55f2955b503fa0d95d8d662a0423c46d7
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2019-02-12 03:07:35 +0900

    ひとまずgetTimeTableでjson形式のレスポンスを得られるようになった

--- getTimeTable.py
+++ getTimeTable.py
@@ -2,27 +2,87 @@ import io,sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
-import requests
+from selenium import webdriver
+import chromedriver_binary
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+import json
 
-USER = "TP00000"
-PASS = "xxxxxxxx"
+sixDays = ["月", "火", "水", "木", "金", "土"]
+jigen = ["1~2限", "3~4限", "5~6限", "7~8限", "9~10限", "11~12限", "13~14限", "15~16限"]
 
-# セッションを開始
-session = requests.session()
+def indexToDay(index):
+    return sixDays[index%6]
 
-# ログイン
-login_info = {
-    "userId":USER,
-    "password":PASS,
-    "buttonName":"",
-    "lang":1
-}
+def indexToJigen(index):
+    return jigen[int(index/6)]
 
-# action
-url_login = "https://tora-net.sti.chubu.ac.jp/portal/top.do"
-res = session.post(url_login, data=login_info)
-res.raise_for_status() # エラーならここで例外を発生させる
+def getTimeTable():
 
-print(res.text)
+    options = webdriver.chrome.options.Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+
+    # ブラウザ起動
+    driver = webdriver.Chrome(chrome_options=options, service_log_path="./chromedriver.log")
+    driver.implicitly_wait(1)
+
+    # ログイン
+    driver.get("https://tora-net.sti.chubu.ac.jp/portal/top.do")
+    driver.find_element_by_id("userId").send_keys("tp00000")
+    driver.find_element_by_id("password").send_keys("xxxxxxxx")
+    driver.find_element_by_xpath("//*[@id='loginButton']").click()
+
+    # 時間割ページへ直接移動
+    driver.get("https://tora-net.sti.chubu.ac.jp/portal/prtlmjkr.do?clearAccessData=true&amp;contenam=prtlmjkr&amp;kjnmnNo=18&#10;")
+    try:
+        driver.find_element_by_xpath('//*[@id="nav"]/li[4]/a').is_displayed()
+    except:
+        dict401 = {"status" : 401}
+
+        driver.quit()
+        JSON = json.dumps([dict401, {"response" : []}], ensure_ascii=False, indent=2)
+        return JSON
+
+    # htmlを取得
+    soup = BeautifulSoup(driver.page_source, "lxml")
+
+
+
+    if len(soup.find_all(class_="jikanwariKoma")) == 0:
+        dict404 = {"status" : 404}
+
+        driver.quit()
+        JSON = json.dumps([dict404, {"response" : []}], ensure_ascii=False, indent=2)
+        return JSON
+
+    # ここまで来ればちゃんと認証通ってる
+    timeTable = []
+    dict200 = {"status" : 200}
+    timeTable.append(dict200)
+    response = []
+
+    # 各コマに対してaタグがあるか精査、あったらtimeTableに各情報を追加
+    for index, koma in enumerate(soup.find_all(class_="jikanwariKoma")):
+        if len(koma.find_all("a")) != 0: # aタグがあったら(講義リンクがあったら)
+            komaTitle, komaRoom, komaTeature, _ = koma.text.replace("\n\n\n", "").split("\n")
+            komaRoom = komaRoom.strip()
+            komaTeature = komaTeature.strip()
+            komaTeature = komaTeature.replace("\u3000", " ")
+            dict = {
+                "day" : indexToDay(index),
+                "jigen" : indexToJigen(index),
+                "komaTitle" : komaTitle,
+                "komaRoom" : komaRoom,
+                "komaTeature" : komaTeature
+            }
+            response.append(dict)
+
+    driver.quit()
+    timeTable.append({"response" : response})
+    JSON = json.dumps(timeTable, ensure_ascii=False, indent=2)
+
+    return JSON
+
+
+if __name__ == '__main__':
+    print(getTimeTable())
--- test/selenium+chrome.py
+++ test/selenium+chrome.py
@@ -4,20 +4,73 @@ sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
 from selenium import webdriver
 import chromedriver_binary
+from bs4 import BeautifulSoup
+import json
+
+sixDays = ["月", "火", "水", "木", "金", "土"]
+jigen = ["1~2限", "3~4限", "5~6限", "7~8限", "9~10限", "11~12限", "13~14限", "15~16限"]
+
+def indexToDay(index):
+    return sixDays[index%6]
+
+def indexToJigen(index):
+    return jigen[int(index/6)]
+
 
 options = webdriver.chrome.options.Options()
-# options.add_argument("--headless")  # これ消せばブラウザ画面が出ます
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
-options.add_argument("--always-authorize-plugins")
-options.add_argument("--javascript-harmony")
-options.add_argument("--user-agent='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Version/3.1 Safari/525.19'")
 
-
+# ブラウザ起動
 driver = webdriver.Chrome(chrome_options=options, service_log_path="./chromedriver.log")
-driver.implicitly_wait(3)
+driver.implicitly_wait(1)
 
+print("ログイン中...")
+
+# ログイン
 driver.get("https://tora-net.sti.chubu.ac.jp/portal/top.do")
+driver.find_element_by_id("userId").send_keys("tp00000")
+driver.find_element_by_id("password").send_keys("xxxxxxxxx")
+driver.find_element_by_xpath("//*[@id='loginButton']").click()
 
-# タイトル
-print(driver.title)
-print(driver.page_source)
+print("時間割情報を取得中...")
+
+# 時間割ページへ直接移動
+driver.get("https://tora-net.sti.chubu.ac.jp/portal/prtlmjkr.do?clearAccessData=true&amp;contenam=prtlmjkr&amp;kjnmnNo=18&#10;")
+try:
+    print(driver.find_element_by_xpath('//*[@id="nav"]/li[4]/a').is_displayed())
+except:
+    print("no such element")
+    dict401 = {"status" : 401}
+
+# htmlを取得
+soup = BeautifulSoup(driver.page_source, "lxml")
+
+# 各コマに対してaタグがあるか精査、あったらtimeTableに各情報を追加
+timeTable = []
+if len(soup.find_all(class_="jikanwariKoma")) == 0:
+    print("ERROR!")
+
+for index, koma in enumerate(soup.find_all(class_="jikanwariKoma")):
+    if len(koma.find_all("a")) != 0: # aタグがあったら(講義リンクがあったら)
+        komaTitle, komaRoom, komaTeature, _ = koma.text.replace("\n\n\n", "").split("\n")
+
+        # komaTeature = komaTeature.replace(" ", "")
+        komaRoom = komaRoom.strip()
+        komaTeature = komaTeature.strip()
+        komaTeature = komaTeature.replace("\u3000", " ")
+
+        dict = {
+            "day" : indexToDay(index),
+            "jigen" : indexToJigen(index),
+            "komaTitle" : komaTitle,
+            "komaRoom" : komaRoom,
+            "komaTeature" : komaTeature
+        }
+
+        timeTable.append(dict)
+
+JSON = json.dumps(timeTable, ensure_ascii=False, indent=2)
+print(JSON)
+
+driver.quit()
